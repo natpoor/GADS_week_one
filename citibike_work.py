188a39bc090865500e4e5d207470d7cd08bf9468
natpoor <natpoor@gmail.com>
2013-11-11 13:28:22 -0500

    Code gets column order correct.
    Improved and cleaned code to get the CSV headers in the correct
    (desired and sensible) order.

--- citibike_work.py
+++ citibike_work.py
@@ -13,37 +13,38 @@ import csv			# Ah right.
 # Variables
 actual_dict = dict()
 file_name = 'citibike.json'
-file_loc = '~/Dropbox/GA_Data_Science/'
-the_file = file_loc + file_name				# Because it's clear that way.
-the_file = os.path.expanduser(the_file)		# Maybe? Please? PITA.
+file_loc = '~/Dropbox/GA_Data_Science/GADS_week_one/'	# Moved for github
+the_file = file_loc + file_name							# Because it's clear that way.
+the_file = os.path.expanduser(the_file)					# Ah important.
 
 output_filename = 'my_citibike.csv'
 output_file = os.path.expanduser(file_loc + output_filename)
 
+these_fields = ['id', 'stationName', 'statusKey', 'availableBikes', 'availableDocks', 'totalDocks', 'latitude', 'longitude']
+		# These fields, not some other fields, and in this order.
 
-# Script no fancy function stuff.
+
+# Linear script, no fancy function stuff.
 with open(the_file, 'r') as f:
 	bike_data = json.load(f)
 print 'Opened and read file.'
-# print bike_data							# Ok!
 
+# print bike_data											# Ok!
 # print json.dump(bike_data, sort_keys=True, indent=1)
-
-# print "Type of bike_data object: ", type(bike_data) 	# Ok is dict
+# print "Type of bike_data object: ", type(bike_data)	 	# Ok is dict.
 
 
 with open(output_file, 'w') as f:
-	x = 1
-	bike_riter = csv.writer(f) 		# Bike riter! Get it?
-	pita_object = bike_data.get('stationBeanList')
-#	print 'Type of actual_dict is: ', type(actual_dict)		# List, what? List of dicts where the keys are all the same.
-	for thing in pita_object:
-		thing = dict(thing)
-#		print thing
-		if (x == 1):
-			bike_riter.writerow(thing.keys())
-			x = x + 1
-		bike_riter.writerow(thing.values())
+	bike_riter = csv.writer(f) 			# Bike riter! Get it?
+	bike_riter.writerow(these_fields)	# Initing the header.
 
-print 'DONE FINALLY!'
+	list_of_dicts = bike_data.get('stationBeanList')			# The actual dicts are in a sub-dict (a list of dicts).
+#	print 'Type of list_of_dicts is: ', type(list_of_dicts)		# List of dicts where the keys are all the same.
+	for little_dict in list_of_dicts:
+		correct_line = []									# Blank the new correct line each time, init.
+		for item in these_fields:
+			correct_line.append(little_dict[item])			# Build line from wanted items in order.
+		bike_riter.writerow(correct_line)					# Writes the line.
+
+print 'Done!'
 
